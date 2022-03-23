6a114f5399a5a48ed14d4d29fbce1a609366a228
echohanz <102195482+echohanz@users.noreply.github.com>
2022-03-22 17:38:16 -0700

    Add files via upload

---
+++ client/src/app/components/about/about.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService} from '../../services/spotify.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  name:string = null;
+  profile_pic:string = "../../../assets/unknown.jpg";
+  profile_link:string = null;
+
+  //TODO: inject the Spotify service
+  constructor(private SpotifyService:SpotifyService) { }
+
+  ngOnInit() {
+  }
+
+  /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
+  In that function, update the name, profile_pic, and profile_link fields */
+public aboutme(): void{
+	this.SpotifyService.aboutMe().then(
+	temp => {
+		this.name = temp.name;
+		this.profile_link = temp.spotifyProfile;
+		this.profile_pic = temp.imageURL;
+	}
+	);
+}
+}
